8fec0ef5b9b3c399844dd671f40d7b14a861df52
Jyri Turku <jyri.turku@student.saimia.fi>
2019-11-10 11:15:09 +0200

    Code fully functional, commented and organized. You are now free to gamble.

--- Lottery/Lottery/Program.cs
+++ Lottery/Lottery/Program.cs
@@ -6,6 +6,7 @@ namespace Lottery
     {
         static void Main(string[] args)
         {
+            //Alustetaan arvonnassa tarvittavat muuttujat sekä tulostaulukko
             int pick;
             int extra = 0;
             int doubleUp = 0;
@@ -13,6 +14,9 @@ namespace Lottery
             Random rnd = new Random();
 
             //Seitsemän päänumeron arvonta
+            #region Main draw
+            //Looppi käydään läpi 7 kertaa, enemmän jos arvotaan sama numero
+            //Arvotaan numero ja tarkistetaan ettei sitä ole jo valittu, lisätään tulostaulukkoon
             for (int i = 0; i < 7; i++)
             {
                 pick = (rnd.Next(40) + 1);
@@ -25,8 +29,12 @@ namespace Lottery
                 }
             }
             Array.Sort(numbers);
+            #endregion
 
             //Lisänumeron arvonta
+            #region Extra
+            //Arvotaan lisänumero, varmistetaan ettei arvottua numeroa ole tulostaulukossa
+            //Jos on, arvotaan uudestaan kunnes löytyy käyttämätön luku
             do
             {
                 pick = (rnd.Next(40) + 1);
@@ -41,8 +49,12 @@ namespace Lottery
                 }
 
             } while (extra == 0);
+            #endregion
 
             //Tuplausnumeron arvonta
+            #region Double
+            //Arvotaan tuplausnumero, varmistetaan ettei arvottua luku ole vielä tulostaulukossa
+            //Jos on, arvotaan uudestaan kunnes löytyy käyttämätön luku
             do
             {
                 pick = (rnd.Next(40) + 1);
@@ -56,9 +68,12 @@ namespace Lottery
                     doubleUp = pick;
                 }
             } while (doubleUp == 0);
+            #endregion
 
+            #region Print out
             //Arvontojen tulostus
             Console.WriteLine("Loton oikearivi on:");
+            //Toistorakenne varmistaa mallin mukaisen kirjoitusasun lottoriville
             foreach (int number in numbers)
             {
                 if (number == numbers[6])
@@ -70,9 +85,10 @@ namespace Lottery
                     Console.Write(number + ", ");
                 }
             }
-
+            //Lisä- ja tuplausnumero tulostetaan mallin mukaisesti omillaan'
             Console.WriteLine("Lisänumero: " + extra);
             Console.WriteLine("Tuplausnumero: " + doubleUp);
+            #endregion
         }
 
         //Tarkistaa onko annetussa taulukossa jo annettu luku. Palauttaa bool arvon "true" jos on.
@@ -87,7 +103,6 @@ namespace Lottery
                     result = true;
                 }
             }
-            Console.WriteLine(number);
             return result;
         }
     }
